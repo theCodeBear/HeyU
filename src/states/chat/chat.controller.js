15a57befbdd6591a22856e5c106d9265859f7deb
Todd Kronenberg <toddkronenberg@gmail.com>
2015-12-04 04:23:05 -0600

    Clean up chat controller. Delete settings service.

--- src/states/chat/chat.controller.js
+++ src/states/chat/chat.controller.js
@@ -4,18 +4,18 @@ angular.module('close')
 
 .controller('ChatCtrl', ChatCtrl);
 
-ChatCtrl.$inject = ['$timeout', 'User', 'Settings', 'Socket', '$interval', '$cordovaGeolocation'];
+ChatCtrl.$inject = ['$timeout', 'User', 'Socket', '$interval', '$cordovaGeolocation'];
 
-function ChatCtrl($timeout, User, Settings, Socket, $interval, $cordovaGeolocation) {
+function ChatCtrl($timeout, User, Socket, $interval, $cordovaGeolocation) {
 
   let vmChat = this;
 
+  const CHAT_TIME_AMOUNT = 600; // seconds
   let mySocketId;
   let theirSocketId;
   let intervalTimer;
   let coords = {};
   let posOptions = {timeout: 10000, enableHighAccuracy: true};
-  const CHAT_TIME_AMOUNT = 600;
   vmChat.theirName = null;
   vmChat.tick = CHAT_TIME_AMOUNT;
   vmChat.username = User.get();
@@ -28,30 +28,22 @@ function ChatCtrl($timeout, User, Settings, Socket, $interval, $cordovaGeolocati
   vmChat.isThisUser = (name) => (vmChat.username === name) ? true : false;
 
 
-  Socket.on('user connected', (socketId) => {
-    mySocketId = socketId;
-    console.log('my socket', mySocketId);
-  });
-
-  Socket.on('receive message', (msg) => {
-    vmChat.messageHistory.push(msg);
-  });
-
-  Socket.on('somone is available', () => {
-    lookingForSomeone();
+  Socket.on('user connected', (socketId) => mySocketId = socketId);
+  Socket.on('receive message', (msg) => vmChat.messageHistory.push(msg));
+  Socket.on('somone is available', () => lookingForSomeone());
+  Socket.on('chat is over', () => ditchChat(false));
+  Socket.on('user got disconnected', () => {
+    alert('Other user was disconnected :(');
+    ditchChat(false);
   });
 
   function lookingForSomeone() {
     console.log('sending step 1');
     // Sending step 1 in finding person, user array in callback
     Socket.emit('finding people', mySocketId, (liveUsers, usersCoords) => {
-      console.log(liveUsers);
-      console.log(liveUsers[Math.floor(Math.random() * liveUsers.length)]);
-      console.log('users coords', usersCoords);
       if (liveUsers.length === 0) return;
       // filter people array by distance, returns distances array to match people array.
       let userDistances = filterUsersForDistance(liveUsers, usersCoords, vmChat.distanceSelected);
-      console.log('userDistances', userDistances);
       let pickedIndex = Math.floor(Math.random() * liveUsers.length);
       let otherUserSocketId = liveUsers[pickedIndex];
       if (otherUserSocketId) {
@@ -67,51 +59,15 @@ function ChatCtrl($timeout, User, Settings, Socket, $interval, $cordovaGeolocati
         });
       } else {
         $timeout(() => {
-      if (!theirSocketId) lookingForSomeone();
-    }, Math.random() * 700 + 300);
+          if (!theirSocketId) lookingForSomeone();
+        }, Math.random()*700+300);
       }
     });
-  }
-
-  function filterUsersForDistance(users, userCoords, distanceMax) {
-    let distances = [];
-    let miles;
-    // later might want to optimize this by using a normal for-loop and only grab
-    // the first, say, 100 users who are within the distance range. This optimization
-    // of course would only be needed if this app were super popular and had thousands
-    // of people using it within the short distance ranges it allows, so very unlikely.
-    users = users.filter((el, i) => {
-      miles = getDistanceFromCoords(coords.lat, coords.lon, userCoords[i].lat, userCoords[i].lon);
-      console.log('miles', miles);
-      if (miles <= +distanceMax) {
-        distances.push(miles);
-        return true;
-      } else
-        return false;
-    });
-    return distances;
-  }
-
-  // given two pairs of geo-coords, returns distance in miles
-  function getDistanceFromCoords(lat1, lon1, lat2, lon2) {
-    const earthRadius = 3959;   // in miles
-    let dLat = degToRadians(lat2 - lat1);
-    let dLon = degToRadians(lon2 - lon1);
-    let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(degToRadians(lat1)) * Math.cos(degToRadians(lat2)) *
-            Math.sin(dLon/2) * Math.sin(dLon/2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    return earthRadius * c;
-  }
-
-  function degToRadians(deg) {
-    return deg * (Math.PI / 180);
   }
 
   // receiving step 3 in finding person
   Socket.on('can you connect', (ids) => {
     console.log('received step 3');
-    console.log('theirSocketId', theirSocketId);
     if (!theirSocketId && ids.distance <= vmChat.distanceSelected) {
       theirSocketId = ids.sendingId;
       vmChat.theirName = ids.sendingName;
@@ -128,10 +84,7 @@ function ChatCtrl($timeout, User, Settings, Socket, $interval, $cordovaGeolocati
       // This timeout fixes a bug in which the client would stop looking for
       // a connection sometimes, not sure the cause of the bug, but this fixes it.
       $timeout(() => {
-        if (!vmChat.chatFound && theirSocketId) {
-          console.log('******** IN 2 SECOND TIMEOUT');
-          theirSocketId = null;
-        }
+        if (!vmChat.chatFound && theirSocketId) theirSocketId = null;
       }, 2000);
     }
   });
@@ -154,14 +107,6 @@ function ChatCtrl($timeout, User, Settings, Socket, $interval, $cordovaGeolocati
     startChatCountdown();
   });
 
-  Socket.on('chat is over', () => {
-    ditchChat(false);
-  });
-
-  Socket.on('user got disconnected', () => {
-    alert('Other user was disconnected :(');
-    ditchChat(false);
-  });
 
   // runs the clock countdown
   function startChatCountdown() {
@@ -187,15 +132,11 @@ function ChatCtrl($timeout, User, Settings, Socket, $interval, $cordovaGeolocati
 
   function selectDistance(distance) {
     vmChat.distanceSelected = distance;
-    Settings.setRange(distance);
-    // addIdToAvailableList(coords);
     getCoordinates();
-    // lookingForSomeone();
   }
 
   function getCoordinates() {
     $cordovaGeolocation.getCurrentPosition(posOptions).then((position) => {
-      console.log(position);
       coords.lat = position.coords.latitude;
       coords.lon = position.coords.longitude;
       addIdToAvailableList(coords);
@@ -220,5 +161,38 @@ function ChatCtrl($timeout, User, Settings, Socket, $interval, $cordovaGeolocati
     vmChat.messageHistory = [];
   }
 
+  function filterUsersForDistance(users, userCoords, distanceMax) {
+    let distances = [];
+    let miles;
+    // later might want to optimize this by using a normal for-loop and only grab
+    // the first, say, 100 users who are within the distance range. This optimization
+    // of course would only be needed if this app were super popular and had thousands
+    // of people using it within the short distance ranges it allows, so very unlikely.
+    users = users.filter((el, i) => {
+      miles = getDistanceFromCoords(coords.lat, coords.lon, userCoords[i].lat, userCoords[i].lon);
+      if (miles <= +distanceMax) {
+        distances.push(miles);
+        return true;
+      } else
+        return false;
+    });
+    return distances;
+  }
+
+  // given two pairs of geo-coords, returns distance in miles
+  function getDistanceFromCoords(lat1, lon1, lat2, lon2) {
+    const earthRadius = 3959;   // in miles
+    let dLat = degToRadians(lat2 - lat1);
+    let dLon = degToRadians(lon2 - lon1);
+    let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(degToRadians(lat1)) * Math.cos(degToRadians(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return earthRadius * c;
+  }
+
+  function degToRadians(deg) {
+    return deg * (Math.PI / 180);
+  }
 
 }
